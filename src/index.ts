e233cb39b87c517551c959269bde8b828d3e9e57
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2021-11-18 14:00:22 +0100

    feat(useToolbar): add useToolbar for managing PF4 filters

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './components/StatusIcon';
 export * from './hooks/useDelete';
 export * from './hooks/useModal';
 export * from './hooks/useTableControls';
+export * from './hooks/useToolbar';
